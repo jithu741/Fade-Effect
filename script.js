fb1806ab0627187964484c1e8b16fe0c8ca561f8
jithu741 <65328514+jithu741@users.noreply.github.com>
2022-01-02 10:55:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const displayText = document.querySelector('.text')
+const backGround = document.querySelector('.background')
+
+let loadingValue = 0
+let timeFrame = setInterval(fade, 70)
+
+function fade() {
+  loadingValue++
+  if (loadingValue> 99){
+    clearInterval(timeFrame)
+  }
+
+  displayText.innerText = `${loadingValue}%`
+  displayText.style.opacity = scale(loadingValue, 0, 100, 1, 0)
+  backGround.style.filter = `blur(${scale(loadingValue, 0, 100, 30, 0)}px)`
+
+}
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
